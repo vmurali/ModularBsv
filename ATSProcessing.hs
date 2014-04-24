7f2cc5848d9387499e77e66f5d50402c6ca9cf9a
Thomas Bourgeat <thomas.07fr@gmail.com>
2014-04-24 13:36:12 -0400

    Work in progress, it compiles. It's roughly finished, I need to test and fix the bugs...

--- ATSProcessing.hs
+++ ATSProcessing.hs
@@ -16,6 +16,8 @@ import Text.Parsec hiding (token)
 import Text.Parsec.String
 import Text.Parsec.Prim
 
+import Debug.Trace
+
 instance Ord a => JoinSemiLattice (Set.Set a) where
 	join = Set.union	
 
@@ -39,11 +41,14 @@ type MethodSet = Set.Set Method
 buildEnv :: Module -> Env
 buildEnv m = List.foldl (\env b -> Map.insert (bindName b) b env) Map.empty $ bindings m
 
-methodCalledMExpr :: MExpression -> (String, String) 
-methodCalledMExpr expr = (calledModule expr, calledMethod expr)
+methodCalledMExpr :: Env -> MExpression -> Set.Set (String, String)   --I add all the methods called inside the if binding
+methodCalledMExpr env expr = case calledCond expr of
+	Nothing -> Set.singleton (calledModule expr, calledMethod expr)
+	Just a ->  Set.singleton (calledModule expr, calledMethod expr) `Set.union` methodsCalledInBinding env (env Map.! a) 
 
-methodsCalled :: Method -> Set.Set (String, String)
-methodsCalled m = Set.fromList . map methodCalledMExpr $ methodBody m
+
+methodsCalled :: Env -> Method -> Set.Set (String, String)
+methodsCalled env m = Set.unions . map (methodCalledMExpr env) $ methodBody m
 
 expandBindingToString :: Binding -> Set.Set String
 expandBindingToString bind = Set.fromList $ (bindName bind : case bindExpr bind of
@@ -118,18 +123,19 @@ methodsCalledByMethod :: Env -> Method -> Set.Set (String, String)
 methodsCalledByMethod env meth = case methodType meth of
 	Value n -> methodsCalledInBinding env (env Map.! (methodName meth)) `Set.union` methodsCalledInBinding env (env Map.! ("RDY_" ++ methodName meth)) 
 	Value0 n -> methodsCalledInBinding env (env Map.! (methodName meth)) `Set.union` methodsCalledInBinding env (env Map.! ("RDY_" ++ methodName meth)) 
-	Action -> methodsCalledInBinding env (env Map.! ("RDY_" ++ methodName meth)) `Set.union`  methodsCalled meth 
+	Action -> methodsCalledInBinding env (env Map.! ("RDY_" ++ methodName meth)) `Set.union`  methodsCalled env meth 
 	ActionValue n -> methodsCalledInBinding env (env Map.! ("RDY_" ++ methodName meth)) `Set.union` 
 			methodsCalledInBinding env (env Map.! (methodName meth)) `Set.union`
-			methodsCalled meth 
+			methodsCalled env meth 
 
-type InstanceName = String
-type MethodName = String
-type RuleOrMethName = String
-type Condition = String
-type Argument = String
-methodsCalledRulesOrMethods :: Module -> Map RuleOrMethName {-->-} (Set.Set (InstanceName, MethodName))
-callerInformation :: Module -> Map (InstanceName, MethodName) {-->-} (Set.Set (Name, Condition, [Argument]))
+
+--type InstanceName = String
+--type MethodName = String
+--type RuleOrMethName = String
+--type Condition = String
+--type Argument = String
+--methodsCalledRulesOrMethods :: Module -> Map RuleOrMethName {-->-} (Set.Set (InstanceName, MethodName))
+--callerInformation :: Module -> Map (InstanceName, MethodName) {-->-} (Set.Set (Name, Condition, [Argument]))
 
 --
 --REVERSE THING : 
@@ -147,32 +153,144 @@ callerInformation :: Module -> Map (InstanceName, MethodName) {-->-} (Set.Set (N
 --
 --b) What do you mean? The last if statement conditionning that trigger this call?
 --
---
+
+
+
 --
 --TODO RULES
 --
 
-giveMeAllTheArgumentOfThisMethod = (String, String) -> Module -> [[ String ]]
-giveMeAllTheArgumentOfThisMethod names mod = argsMethodCallInsideMethods names (methods mod) ++ argsMethodCallInsideBindings names (bindings mod)
+--
+--TODO : If you prefer a map, we can build the map the fromSet function
+--
+
+-- 
+--TODO : LIFT THE NAMES AND THE ORDER OF THE ARGUMENTS OF MY FUNCTIONS 
+--	
 
 
-argsMethodCallInsideMethods :: (String, String) -> [ Method ] -> [[ String ]]
-argsMethodCallInsideMethods name l = catMaybes . map (\x -> argsMethodCallInsideBody name x) $ l   --Yep we could use mapMaybes TODO
 
-argsMethodCallInsideBindings :: (String, String) -> [ Binding ] -> [[ String ]]
-argsMethodCallInsideBindings name l = catMaybes . map (\x -> argsMethodCallInsideBinding name x) $ l   --Yep we could use mapMaybes TODO
+-- We use [[ String ]] but actually it cannot contains more than one element. TODO : Another datastructure ? -> Dunno, great haskell backend for lists 
 
-argsMethodCallInsideBody :: (String, String) -> Method -> [[ String ]]
-argsMethodCallInsideBody name meth = catMaybes . map (\x -> argsMethodCallInsideMExpr name x) $ methodBody meth
+callerInformation :: Module -> Env -> (String, String) -> [ (String, Maybe String, [String])] --Why the Set datastructure? Perf? Think about that. TODO
+callerInformation mod env names = 
+	Maybe.catMaybes . map (\method -> let called = argsMethodCallInsideMethod env names method in
+						case called of
+							[] -> Nothing
+							[a] ->let theCond = catchTheCond env names method in
+								case theCond of 
+									[] -> Just (methodName method, Nothing, a)
+									[b] -> Just (methodName method, Just b, a) 
+									_ -> trace "False assumption l 182" $ undefined 
+							_ -> undefined ) $ methods mod
 
-argsMethodCallInsideBinding :: (String, String) -> Binding -> Maybe ([ String ])
-argsMethodCallInsideBinding (nameMod, nameMet) bind = case bindExpr bind of
-		MCall l -> if calledModule l == nameMod && calledMethod l == nameMet then Just $ calledArgs l else Nothing
-		_ -> Nothing 	
+catchTheCond :: Env -> (String, String) -> Method -> [String]
+catchTheCond env names method = (Maybe.catMaybes . map (extractCondFromMExpr env names) $ methodBody method)
+						++ Set.elems (extractCondFromBind env names (Set.empty) (env Map.! ("RDY_" ++ methodName method)))
+						++ case methodType method of
+							Action -> []
+							_ -> Set.elems (extractCondFromBind env names (Set.empty) (env Map.! methodName method))
+
+extractCondFromMExpr :: Env -> (String, String) -> MExpression ->  Maybe String  --I assume our hypothesis here. BIG ISSUE if it's false.
+extractCondFromMExpr env (nMod, nMet) expr = case calledCond expr of
+	Nothing -> Nothing
+	Just a ->  if (nMod, nMet) == (calledModule expr ,calledMethod expr) then Just a else Nothing
 
  
-argsMethodCallInsideMExpr :: (String, String) -> MExpression -> Maybe ([ String ])
-argsMethodCallInsideMExpr 
+extractCondFromBind :: Env -> (String, String) -> Set.Set String -> Binding ->  Set.Set String   --Fucking greedy. 
+extractCondFromBind env names path bind = case bindExpr bind of
+	 MCall m -> if names == (calledModule m, calledMethod m) then path else Set.empty
+	 Mux a b c -> Set.unions . map (extractCondFromBind env names (Set.insert a path)) $ [ env Map.! b, env Map.! c]
+	 _ -> Set.unions . map ((extractCondFromBind env names path) . (env Map.!))$ case bindExpr bind of 
+		 SConst a -> []
+		 Sext a ->  [a]
+		 Not a -> [a]
+		 BNot a ->  [a]
+		 Renaming a -> [a]
+		 Plus a b ->  [a,b]
+		 Times a b ->  [a,b]
+		 Divide a b ->  [a,b]
+		 Modulus a b ->  [a,b]
+		 Lt a b ->  [a,b]
+		 Gt a b  ->  [a,b]
+		 LtEq a b ->  [a,b]
+		 GtEq a b  ->  [a,b]
+		 ULt a b ->  [a,b]
+		 UGt a b ->  [a,b]
+		 PrimArrayDynSelect a b ->  [a,b]
+		 ULtEq a b ->  [a,b]
+		 UGtEq a b  ->  [a,b]
+		 BAnd a b ->  [a,b]
+		 BOr a b ->  [a,b]
+		 RShift a b  ->  [a,b]
+		 LShift a b ->  [a,b]
+		 BXor a b ->  [a,b]
+		 NotEqual a b ->  [a,b]
+		 Minus a b ->  [a,b]
+		 Equal a b ->  [a,b]
+		 R3Shift a b ->  [a,b]
+		 L3Shift a b ->  [a,b]
+ 		 Or a b ->  [a,b]
+		 And a b ->  [a,b]
+		 Extract a b c ->  [a,b,c]
+		 Concat l ->  l
+	   
+
+argsMethodCallInsideMethod :: Env -> (String, String) -> Method -> [[ String ]] -- The list contains at most one sublist
+argsMethodCallInsideMethod env names meth =  (concat . map (argsMethodCallInsideMExpr env names) $ methodBody meth) ++ --Perhaps inside the body
+	(argsMethodCallInsideBinding env names $ env Map.! ("RDY_" ++ methodName meth)) ++  --Perhaps It's called inside the ready statement
+ 	case methodType meth of  -- Or inside the result
+		Value n -> argsMethodCallInsideBinding env names $ env Map.! methodName meth
+		Value0 n -> argsMethodCallInsideBinding env names $ env Map.! methodName meth
+		Action -> []
+		ActionValue n -> argsMethodCallInsideBinding env names $ env Map.! methodName meth
+
+
+argsMethodCallInsideMExpr :: Env -> (String, String) -> MExpression -> [[ String ]]
+argsMethodCallInsideMExpr env names expr = (if (calledModule expr,calledMethod expr) == names then [calledArgs expr] else []) ++ 
+ 	case calledCond expr of
+		Nothing -> []
+		Just a -> argsMethodCallInsideBinding env names (env Map.! a)
+
+
+argsMethodCallInsideBinding :: Env -> (String, String) -> Binding -> [[ String ]]
+argsMethodCallInsideBinding env (nMod,nMet) bind = case (bindExpr bind) of
+	MCall m -> if (calledModule m,calledMethod m) == (nMod, nMet) then [ calledArgs m ] else []
+	_ -> concat . map (argsMethodCallInsideBinding env (nMod,nMet) . (env Map.!)) $
+		case bindExpr bind of 
+			SConst a -> []
+			Sext a ->  [a]
+			Not a -> [a]
+			BNot a ->  [a]
+			Renaming a -> [a]
+			Plus a b ->  [a,b]
+			Times a b ->  [a,b]
+			Divide a b ->  [a,b]
+			Modulus a b ->  [a,b]
+			Lt a b ->  [a,b]
+			Gt a b  ->  [a,b]
+			LtEq a b ->  [a,b]
+			GtEq a b  ->  [a,b]
+			ULt a b ->  [a,b]
+			UGt a b ->  [a,b]
+			PrimArrayDynSelect a b ->  [a,b]
+			ULtEq a b ->  [a,b]
+			UGtEq a b  ->  [a,b]
+			BAnd a b ->  [a,b]
+			BOr a b ->  [a,b]
+			RShift a b  ->  [a,b]
+			LShift a b ->  [a,b]
+			BXor a b ->  [a,b]
+			NotEqual a b ->  [a,b]
+			Minus a b ->  [a,b]
+			Equal a b ->  [a,b]
+			R3Shift a b ->  [a,b]
+			L3Shift a b ->  [a,b]
+ 			Or a b ->  [a,b]
+			And a b ->  [a,b]
+			Mux a b c ->  [a,b,c]
+			Extract a b c ->  [a,b,c]
+	 		Concat l ->  l
 
 
 
