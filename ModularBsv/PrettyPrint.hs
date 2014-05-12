641c5382b26c4622e63d53491059aa0774c49398
Thomas Bourgeat <thomas.07fr@gmail.com>
2014-05-12 13:30:55 -0400

    Work in progress, not very productive today

--- ModularBsv/PrettyPrint.hs
+++ ModularBsv/PrettyPrint.hs
@@ -1,4 +1,22 @@
 import DataTypes
 
-prettyPrint (bn, Binding ba bz bexpr) =
-  "wire " ++ bn ++ "[" ++ ba ++ "-1:0][" ++ bz ++ "-1:0]\nassign " ++ bn ++ " = " prettyPrint bexpr
+prettyPrintBind (bn, Binding ba bz bexpr) =
+  "wire " ++ bn ++ "[" ++ ba ++ "-1:0][" ++ bz ++ "-1:0]\nassign " ++ bn ++ " = " ++
+  prettyPrintExp bexpr
+
+
+prettyPrintExp (Expr op listArgs) = case op of 
+   None -> intercalate listArgs ' '  
+   Unary s -> s ++ intercalate listArgs " "
+   Binary s -> intercalate listArgs $ " " ++ s ++ " "
+   Word s -> intercalate listArgs $ " " ++ s ++ " "
+   MethCall CalledMethod -> undefined
+
+prettyPrintSched (x,y) l =
+	"assign EN_" ++ y ++ " = "
+	intercalate
+		(map (\(a,b) -> "! (" ++ print (a,b) ++ ")" ) l)	
+		" && "
+	where
+		printe (a,b) = if a == "fp" || a == "this" 
+				then "EN_"++b else "EN_" ++ a ++ "_" ++ b 
