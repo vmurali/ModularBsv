08fe321cbbeb0d9bebdbb5d41860ba878b362d19
Thomas Bourgeat <thomas.07fr@gmail.com>
2014-05-12 14:30:12 -0400

    Doesn't type check yet. Fort stupid reason

--- ModularBsv/PrettyPrint.hs
+++ ModularBsv/PrettyPrint.hs
@@ -1,22 +1,45 @@
 import DataTypes
+import Data.Map as Map
+import Data.List as List
+
+prettyPrint (mName,
+		mapBinds,
+		_,
+		_,
+		_,
+		_,
+		_,
+		schedulerInf) =
+	List.foldl (\acc (n,bind) -> acc ++ prettyPrintBind (n,bind))   
+		[]
+		$ Map.toList mapBinds 
+	++
+	List.foldl (\acc (n,l) -> acc ++ prettyPrintSched n l) 
+		[]
+		$ Map.toList schedulerInf 
+
+
 
 prettyPrintBind (bn, Binding ba bz bexpr) =
   "wire " ++ bn ++ "[" ++ ba ++ "-1:0][" ++ bz ++ "-1:0]\nassign " ++ bn ++ " = " ++
-  prettyPrintExp bexpr
+  prettyPrintExp bexpr ++ "\n"
 
 
 prettyPrintExp (Expr op listArgs) = case op of 
-   None -> intercalate listArgs ' '  
-   Unary s -> s ++ intercalate listArgs " "
-   Binary s -> intercalate listArgs $ " " ++ s ++ " "
-   Word s -> intercalate listArgs $ " " ++ s ++ " "
-   MethCall CalledMethod -> undefined
+   None -> intercalate " " listArgs  
+   Unary s -> s ++ intercalate " " listArgs 
+   Binary s -> intercalate (" " ++ s ++ " ") listArgs
+   Word s -> intercalate (" " ++ s ++ " ") listArgs
+   MethCall c-> undefined
 
 prettyPrintSched (x,y) l =
-	"assign EN_" ++ y ++ " = "
+	"assign EN_" ++ y ++ " = " ++
 	intercalate
-		(map (\(a,b) -> "! (" ++ print (a,b) ++ ")" ) l)	
 		" && "
+		(List.map (\(a,b) -> "! (" ++ printTerm (a,b) ++ ")" ) l)	
+	++ "\n"
 	where
-		printe (a,b) = if a == "fp" || a == "this" 
+		printTerm (a,b) = if a == "fp" || a == "this" 
 				then "EN_"++b else "EN_" ++ a ++ "_" ++ b 
+
+
