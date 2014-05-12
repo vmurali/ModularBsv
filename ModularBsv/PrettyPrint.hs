05b03d96f9636c77a78bf72ed07764b1548137f3
Thomas Bourgeat <thomas.07fr@gmail.com>
2014-05-12 17:06:39 -0400

    It compiles now

--- ModularBsv/PrettyPrint.hs
+++ ModularBsv/PrettyPrint.hs
@@ -1,35 +1,33 @@
 import DataTypes
 import Data.Map as Map
 import Data.List as List
+import Control.Applicative
 
 prettyPrint (mName,
 		mapBinds,
 		mapInsts,
-		_,
-		_,
-		_,
-		_,
+		mapRules,
 		schedulerInf) =
-	concat . map 
+	"module" ++ mName ++ ";\n" ++	
+	concat (List.map 
 		(\(n,bind) -> prettyPrintBindW (n,bind))   
-		(Map.assocs mapBinds) 
+		$ Map.assocs mapBinds)
 	++
-	concat . map --HANDLE THE RULES 
+	concat (List.map 
 		(undefined)
-		undefined
+		$ Map.assocs mapRules)
 	++
-	concat . map 
+	concat (List.map 
 		(\(n,inst) -> prettyPrintInst (n,inst))   
-		(Map.assocs mapInsts ) 
+		$ Map.assocs mapInsts )
 	++
-	concat . map 
+	concat (List.map 
 		(\(n,bind) -> prettyPrintBindA (n,bind))   
-		(Map.assocs mapBinds) 
+		$ Map.assocs mapBinds)
 	++
-	concat . map 
+	concat (List.map 
 		(\(n,l) -> prettyPrintSched n l) 
-		(Map.assocs schedulerInf) 
-
+		$ Map.assocs schedulerInf) 
 
 
 
@@ -39,19 +37,19 @@ prettyPrintInst (name,inst) =
 	(show $ instModule inst) ++
 	case (instWidth inst,instInit inst, instSize inst) of
 		(Expr None _ ,Expr None _ ,Expr None _ ) -> "\n" 
-		otherwise -> "#(" ++ undefined ++")\n" 
+		otherwise -> "#(" ++ (intercalate "," $ [show . instWidth, show . instInit , show . instSize] <*> [inst] ) ++")\n" 
 	++ "("
 	++ intercalate
 		","
-		map 
+		(List.map 
 			(\(x,y)-> x++"_"++y)
-			instArgs
-	++ ")"
+			$ instArgs inst)
+	++ ")\n"
 
 
 
 
-prettyPrintBindW (bn, Binding ba bz bexpr) = --TODO : MAYBE INTEGER  
+prettyPrintBindW (bn, Binding ba bz bexpr) = 
   "wire " ++ bn ++ case ba of 
 			Nothing -> ""
 			Just 0 -> "" 
@@ -61,7 +59,7 @@ prettyPrintBindW (bn, Binding ba bz bexpr) = --TODO : MAYBE INTEGER
 	otherwise -> "[" ++ show bz ++ "-1:0]\n"
 
 
-prettyPrintBindA (bn, Binding ba bz bexpr) = --TODO : MAYBE INTEGER  
+prettyPrintBindA (bn, Binding ba bz bexpr) = 
 	"assign " ++ bn ++ " = " ++
 	prettyPrintExp bexpr ++ "\n"
 
